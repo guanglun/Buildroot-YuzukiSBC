e2301750c035c0d0ba933766ec4d804a387fffcc
guanglun <272073020@qq.com>
2022-04-25 02:43:08 -0700

    add longan rtl8189fs support.

--- buildroot/package/rtl8189fs/rtl8189fs.mk
+++ buildroot/package/rtl8189fs/rtl8189fs.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = e10294d2ded27586149fedee1f834d75477b8b40
-RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
+RTL8189FS_VERSION = 8ab5f57d2b602ed5464598cc7f6d39126ce4d523
+RTL8189FS_SITE = $(call github,guanglun,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 
 RTL8189FS_MODULE_MAKE_OPTS = \
